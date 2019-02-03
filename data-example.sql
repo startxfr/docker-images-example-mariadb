5a464d95a0b0c2cb9cd7bac0f40cf113b6d004bf
startxfr <clarue@startx.fr>
2019-02-03 21:35:34 +0100

    update example for 0.4.0

--- data-example.sql
+++ data-example.sql
@@ -1,6 +1,6 @@
 # add SQL queries to execute when configuring the database
 # see /bin/sx-mariadb usage to activate this feature
 
-LOCK TABLES `user_iphoneConfig` WRITE;
+LOCK TABLES `sample` WRITE;
 INSERT INTO `sample` VALUES (1,'version','0.0.5');
 UNLOCK TABLES;
